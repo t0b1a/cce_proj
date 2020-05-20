cea12d4aa7c640ebf83df8734b19831ccbbbc1bc
Tob <tobia.antoniolli@gmail.com>
2020-05-20 14:45:27 +0200

    home finita

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,4 @@ class Job(models.Model): #per interfacciarsi col db
     image = models.ImageField(upload_to='images/') # inside alla media folder che andremo a creare ci sarà una sottodir images
     title = models.CharField(max_length=200) #mettiamo un cap per lo spazio di testo
     description = models.CharField(max_length=200)
-    url = models.URLField(max_length = 200)
+    url = models.URLField(max_length = 200, default='')
