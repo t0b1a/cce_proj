d169000dced60e0961fc23952d514c0d171ee01d
Tob <tobia.antoniolli@gmail.com>
2020-05-20 13:08:28 +0200

    situazione di partenza

--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,5 @@ from django.db import models
 class Job(models.Model): #per interfacciarsi col db
     image = models.ImageField(upload_to='images/') # inside alla media folder che andremo a creare ci sarà una sottodir images
     title = models.CharField(max_length=200) #mettiamo un cap per lo spazio di testo
-    placeholder = models.CharField(max_length=200)
-    button_text = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+    url = models.URLField(max_length = 200)
